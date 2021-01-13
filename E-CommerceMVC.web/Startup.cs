24ed1f103449bacdf435d80fccb1ad8cbe294147
adnanhargan <adnanhargan101@gmail.com>
2021-01-13 12:03:44 +0500

    Added web Project

---
+++ E-CommerceMVC.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_CommerceMVC.web.Startup))]
+namespace E_CommerceMVC.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
