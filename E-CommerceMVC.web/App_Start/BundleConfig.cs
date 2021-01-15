c026d1093288ac5a451bd880faf67b993aa0d547
adnanhargan <adnanhargan101@gmail.com>
2021-01-15 20:11:01 +0500

    Fashe Template added, Bundling Applied

--- E-CommerceMVC.web/App_Start/BundleConfig.cs
+++ E-CommerceMVC.web/App_Start/BundleConfig.cs
@@ -8,23 +8,37 @@ namespace E_CommerceMVC.web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+          "~/Content/templates/fashe/vendor/bootstrap/css/bootstrap.min.css",
+          "~/Content/templates/fashe/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
+          "~/Content/templates/fashe/fonts/themify/themify-icons.css",
+          "~/Content/templates/fashe/fonts/Linearicons-Free-v1.0.0/icon-font.min.css",
+          "~/Content/templates/fashe/fonts/elegant-font/html-css/style.css",
+          "~/Content/templates/fashe/vendor/animate/animate.css",
+          "~/Content/templates/fashe/vendor/css-hamburgers/hamburgers.min.css",
+          "~/Content/templates/fashe/vendor/animsition/css/animsition.min.css",
+          "~/Content/templates/fashe/vendor/select2/select2.min.css",
+          "~/Content/templates/fashe/vendor/daterangepicker/daterangepicker.css",
+          "~/Content/templates/fashe/vendor/slick/slick.css",
+          "~/Content/templates/fashe/vendor/lightbox2/css/lightbox.min.css",
+          "~/Content/templates/fashe/css/util.css",
+          "~/Content/templates/fashe/css/main.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/Content/templates/fashe/vendor/jquery/jquery-3.2.1.min.js",
+                      "~/Content/templates/fashe/vendor/animsition/js/animsition.min.js",
+                      "~/Content/templates/fashe/vendor/bootstrap/js/popper.js",
+                      "~/Content/templates/fashe/vendor/bootstrap/js/bootstrap.min.js",
+                      "~/Content/templates/fashe/vendor/select2/select2.min.js",
+                      "~/Content/templates/fashe/vendor/slick/slick.min.js",
+                      "~/Content/templates/fashe/js/slick-custom.js",
+                      "~/Content/templates/fashe/vendor/countdowntime/countdowntime.js",
+                      "~/Content/templates/fashe/vendor/lightbox2/js/lightbox.min.js",
+                      "~/Content/templates/fashe/vendor/sweetalert/sweetalert.min.js",
+                      "~/Content/templates/fashe/vendor/parallax100/parallax100.js",
+                      "~/Content/templates/fashe/js/main.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
