b3677c8def6c562de150df5cd592bd2ff55b7729
adnanhargan <adnanhargan101@gmail.com>
2021-01-16 10:53:13 +0500

    Admin Area Created and Sb-Admin2 Template applied

--- E-CommerceMVC.web/App_Start/BundleConfig.cs
+++ E-CommerceMVC.web/App_Start/BundleConfig.cs
@@ -8,6 +8,23 @@ namespace E_CommerceMVC.web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //Admin Dashboard bundles
+            bundles.Add(new StyleBundle("~/bundles/dashboard/css").Include(
+          //"~/Content/templates/sb-admin2/vendor/fontawesome-free/css/all.min.css",
+          "~/Content/templates/sb-admin2/css/sb-admin-2.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dashboard/js").Include(
+                      "~/Content/templates/sb-admin2/vendor/jquery/jquery.min.js",
+                      "~/Content/templates/sb-admin2/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                      "~/Content/templates/sb-admin2/vendor/jquery-easing/jquery.easing.min.js",
+                      "~/Content/templates/sb-admin2/js/sb-admin-2.min.js"
+                      //"~/Content/templates/sb-admin2/vendor/chart.js/Chart.min.js",
+                      //"~/Content/templates/sb-admin2/js/demo/chart-area-demo.js",
+                      //"~/Content/templates/sb-admin2/js/demo/chart-pie-demo.js"
+                      ));
+
+            //User Frontend Bundles
+
             bundles.Add(new StyleBundle("~/bundles/css").Include(
           "~/Content/templates/fashe/vendor/bootstrap/css/bootstrap.min.css",
           //"~/Content/templates/fashe/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
